b4010ec63236103fe3c820bafab7b31ed47a86ea
bronifty <bronifty@gmail.com>
2024-07-30 19:41:51 -0500

    this

---
+++ rspress.config.ts
@@ -0,0 +1,23 @@
+import * as path from 'path';
+import { defineConfig } from 'rspress/config';
+
+export default defineConfig({
+  root: path.join(__dirname, 'docs'),
+  title: 'bronifty',
+  description: 'software blog, docs, and apps showcase',
+  icon: '/bronifty-icon.jpg',
+  logo: {
+    light: '/bronifty-icon.jpg',
+    dark: '/bronifty-icon.jpg',
+  },
+  themeConfig: {
+    socialLinks: [
+      { icon: 'github', mode: 'link', content: 'https://github.com/bronifty' },
+      {
+        icon: 'twitter',
+        mode: 'link',
+        content: 'https://twitter.com/bronifty',
+      },
+    ],
+  },
+});
