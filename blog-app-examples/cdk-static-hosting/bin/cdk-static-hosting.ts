2471501444da75e1aea49da2bd27dcefeb2fd809
bronifty <bronifty@gmail.com>
2024-08-03 17:56:01 -0400

    this

--- blog-app-examples/cdk-static-hosting/bin/cdk-static-hosting.ts
+++ blog-app-examples/cdk-static-hosting/bin/cdk-static-hosting.ts
@@ -3,7 +3,9 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { CdkStaticHostingStack } from "../lib/cdk-static-hosting-stack";
 import { CdkStaticHostingStack as CdkStaticHostingStack2 } from "../lib/cdk-static-hosting-stack2";
+import { CdkStaticHostingStack as CdkStaticHostingStack3 } from "../lib/cdk-static-hosting-stack3";
 
 const app = new cdk.App();
 new CdkStaticHostingStack(app, "CdkStaticHostingStack", {});
 new CdkStaticHostingStack2(app, "CdkStaticHostingStack2", {});
+new CdkStaticHostingStack3(app, "CdkStaticHostingStack3", {});
