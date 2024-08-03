f8a6fcef0d16bab0ea5521efffd7fd5400026a4e
bronifty <bronifty@gmail.com>
2024-08-02 19:01:13 -0500

    this

---
+++ temp.sh
@@ -0,0 +1,11 @@
+
+
+aws lambda add-permission \
+--statement-id "AllowCloudFrontServicePrincipal" \
+--action "lambda:InvokeFunctionUrl" \
+--principal "cloudfront.amazonaws.com" \
+--source-arn "arn:aws:cloudfront::851725517932:distribution/ESIRP741RQG6" \
+--region "us-east-1" \
+--function-name remix-ssr
+
+
