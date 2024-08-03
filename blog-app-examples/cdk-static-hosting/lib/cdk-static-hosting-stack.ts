aff2df36c9d9bbff6728a4dda326fd9591c3b6e8
bronifty <bronifty@gmail.com>
2024-08-03 16:23:13 -0400

    this

--- blog-app-examples/cdk-static-hosting/lib/cdk-static-hosting-stack.ts
+++ blog-app-examples/cdk-static-hosting/lib/cdk-static-hosting-stack.ts
@@ -11,6 +11,56 @@ export class CdkStaticHostingStack extends cdk.Stack {
 
     const bucket = new cdk.aws_s3.Bucket(this, "bronifty-deleteme", {});
 
+    // Create CloudFront OAC
+    const oac = new cdk.aws_cloudfront.CfnOriginAccessControl(this, "OAC", {
+      originAccessControlConfig: {
+        name: "OAC for S3 Static Website",
+        originAccessControlOriginType: "s3",
+        signingBehavior: "always",
+        signingProtocol: "sigv4",
+      },
+    });
+
+    // Create CloudFront distribution
+    const distribution = new cdk.aws_cloudfront.Distribution(
+      this,
+      "Distribution",
+      {
+        defaultBehavior: {
+          origin: new cdk.aws_cloudfront_origins.S3Origin(bucket, {
+            originAccessIdentity: undefined, // Disable OAI
+          }),
+          viewerProtocolPolicy:
+            cdk.aws_cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        },
+        defaultRootObject: "index.html",
+      }
+    );
+
+    // Attach OAC to the distribution
+    const cfnDistribution = distribution.node
+      .defaultChild as cdk.aws_cloudfront.CfnDistribution;
+    cfnDistribution.addPropertyOverride(
+      "DistributionConfig.Origins.0.OriginAccessControlId",
+      oac.getAtt("Id")
+    );
+
+    // Update bucket policy
+    bucket.addToResourcePolicy(
+      new cdk.aws_iam.PolicyStatement({
+        actions: ["s3:GetObject"],
+        resources: [bucket.arnForObjects("*")],
+        principals: [
+          new cdk.aws_iam.ServicePrincipal("cloudfront.amazonaws.com"),
+        ],
+        conditions: {
+          StringEquals: {
+            "AWS:SourceArn": `arn:aws:cloudfront::${this.account}:distribution/${distribution.distributionId}`,
+          },
+        },
+      })
+    );
+
     const deployment = new cdk.aws_s3_deployment.BucketDeployment(
       this,
       "DeployWithCDK",
@@ -21,7 +71,22 @@ export class CdkStaticHostingStack extends cdk.Stack {
           ),
         ],
         destinationBucket: bucket,
+        distribution,
+        distributionPaths: ["/*"],
       }
     );
+
+    // Export the Function URL as a stack output
+    new cdk.CfnOutput(this, "bucketName", {
+      value: bucket.bucketName,
+      description: "name of the S3 bucket",
+      exportName: "bucketName",
+    });
+
+    // Output CloudFront URL
+    new cdk.CfnOutput(this, "DistributionUrl", {
+      value: `https://${distribution.distributionDomainName}`,
+      description: "CloudFront Distribution URL",
+    });
   }
 }
