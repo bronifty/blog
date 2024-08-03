eb1dfa8e246b428f791858c9523b3cc728770ee8
bronifty <bronifty@gmail.com>
2024-08-03 18:19:44 -0400

    this

--- blog-app-examples/cdk-static-hosting/bin/cdk-static-hosting.ts
+++ blog-app-examples/cdk-static-hosting/bin/cdk-static-hosting.ts
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { CdkStaticHostingStack } from "../lib/cdk-static-hosting-stack";
-import { CdkStaticHostingStack as CdkStaticHostingStack2 } from "../lib/cdk-static-hosting-stack2";
-import { CdkStaticHostingStack as CdkStaticHostingStack3 } from "../lib/cdk-static-hosting-stack3";
+// import { CdkStaticHostingStack } from "../lib/cdk-static-hosting-stack";
+// import { CdkStaticHostingStack as CdkStaticHostingStack2 } from "../lib/cdk-static-hosting-stack2";
+// import { CdkStaticHostingStack as CdkStaticHostingStack3 } from "../lib/cdk-static-hosting-stack3";
+import { CdkStaticHostingStack as CdkStaticHostingStack4 } from "../lib/cdk-static-hosting-stack4";
 
 const app = new cdk.App();
-new CdkStaticHostingStack(app, "CdkStaticHostingStack", {});
-new CdkStaticHostingStack2(app, "CdkStaticHostingStack2", {});
-new CdkStaticHostingStack3(app, "CdkStaticHostingStack3", {});
+// new CdkStaticHostingStack(app, "CdkStaticHostingStack", {});
+// new CdkStaticHostingStack2(app, "CdkStaticHostingStack2", {});
+// new CdkStaticHostingStack3(app, "CdkStaticHostingStack3", {});
+new CdkStaticHostingStack4(app, "CdkStaticHostingStack4", {});
