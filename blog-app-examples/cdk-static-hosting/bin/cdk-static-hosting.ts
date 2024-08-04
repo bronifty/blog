61cfe06e59e016288fba096957d1afbdbfa813c1
bronifty <bronifty@gmail.com>
2024-08-04 12:47:03 -0400

    this

--- blog-app-examples/cdk-static-hosting/bin/cdk-static-hosting.ts
+++ blog-app-examples/cdk-static-hosting/bin/cdk-static-hosting.ts
@@ -2,28 +2,16 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { CdkStaticHostingStack } from "../lib/cdk-static-hosting-stack";
-import { execSync } from "node:child_process";
 
-// Function to get AWS account ID
+// Function to get AWS account ID from environment variable
 function getAccountId(): string {
-  const accountId = execSync(
-    "aws sts get-caller-identity --query Account --output text"
-  )
-    .toString()
-    .trim();
-  process.env.AWS_ACCOUNT_ID = accountId || "";
-  return process.env.AWS_ACCOUNT_ID;
+  return process.env.AWS_ACCOUNT_ID || "";
 }
 
-// Function to get AWS region
+// Function to get AWS region from environment variable
 function getRegion(): string {
-  const region = execSync("aws configure get region").toString().trim();
-  process.env.AWS_REGION = region || "";
-  return process.env.AWS_REGION;
+  return process.env.AWS_REGION || "";
 }
-
-// process.env.AWS_ACCOUNT_ID = "851725517932";
-// process.env.AWS_REGION = "us-east-1";
 
 const app = new cdk.App();
 new CdkStaticHostingStack(app, "CdkStaticHostingStack", {
