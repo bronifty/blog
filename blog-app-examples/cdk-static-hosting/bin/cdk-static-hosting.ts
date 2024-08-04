27b274d20ddd5fbb0df7e973741aa22f78f8e9a1
bronifty <bronifty@gmail.com>
2024-08-04 12:49:10 -0400

    this

--- blog-app-examples/cdk-static-hosting/bin/cdk-static-hosting.ts
+++ blog-app-examples/cdk-static-hosting/bin/cdk-static-hosting.ts
@@ -5,11 +5,13 @@ import { CdkStaticHostingStack } from "../lib/cdk-static-hosting-stack";
 
 // Function to get AWS account ID from environment variable
 function getAccountId(): string {
+  console.log(process.env.AWS_ACCOUNT_ID);
   return process.env.AWS_ACCOUNT_ID || "";
 }
 
 // Function to get AWS region from environment variable
 function getRegion(): string {
+  console.log(process.env.AWS_REGION);
   return process.env.AWS_REGION || "";
 }
 
